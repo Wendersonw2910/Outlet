b1259b3b296eef6af7eec7f91562511bb03707ad
Wenderson <wendersondiegobessa1@outlook.com>
2025-10-16 21:54:23 -0300

    metodo para adicionar cliente

--- outlet/src/main/java/outlet/controller/OutletControler.java
+++ outlet/src/main/java/outlet/controller/OutletControler.java
@@ -2,6 +2,8 @@ package outlet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import outlet.domain.Cliente;
 import outlet.services.OutletService;
@@ -15,4 +17,6 @@ public class OutletControler {
     private OutletService outletService;
     @GetMapping("/clientes")
     public List<Cliente> getAllClientes(){return outletService.allClientes();}
+    @PostMapping("/addCliente")
+    public String addCliente(@RequestBody Cliente cliente){return outletService.addClienteService(cliente);}
 }
